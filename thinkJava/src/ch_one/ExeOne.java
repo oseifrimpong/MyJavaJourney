b3002eeed62436426dc435260e039c617e33a9f9
obedDecath <obed.oseifrimpong@decathlon.com>
2019-07-02 06:28:26 +0800

    Done with the first chapter

---
+++ thinkJava/src/ch_one/ExeOne.java
@@ -0,0 +1,16 @@
+/*
+1. Type in the hello world program, then compile and run it.
+
+2. Add a print statement that displays a second message after the “Hello, World!”. Say something witty like, “How are you?” Compile and run the program again.
+
+3. Add a comment to the program (anywhere), recompile, and run it again. The new comment should not affect the result.
+ */
+package ch_one;
+
+public class ExeOne {
+  public static void main (String [] args){
+    System.out.println ("Hello, World!");
+//    I am start Java programming all over again and I feel very positive about this.
+    System.out.println ("How are you?");
+  }
+}
